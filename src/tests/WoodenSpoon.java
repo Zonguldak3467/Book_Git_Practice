223bdf14375b8f757896996babf77b88160cc543
yasinakkaya <yasinakkaya87@gmail.com>
2021-10-03 16:29:22 -0400

    added wooden spoon

---
+++ src/tests/WoodenSpoon.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class WoodenSpoon {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wooden Spoon");
+
+        }
+    }
+}
