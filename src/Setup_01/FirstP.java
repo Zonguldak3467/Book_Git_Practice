f663ae80d06034cdb56f24b13bee3e0e986a4a41
yasinakkaya <yasinakkaya87@gmail.com>
2021-10-03 15:27:50 -0400

    updating my first file

--- src/Setup_01/FirstP.java
+++ src/Setup_01/FirstP.java
@@ -2,6 +2,6 @@ package Setup_01;
 
 public class FirstP {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Universe");
     }
 }
